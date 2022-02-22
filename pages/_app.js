c4c49bd97326d423e700eb7646d1fa217ca32fe0
BrainGhost <juniorbalamage@gmail.com>
2022-02-23 02:58:20 +0300

    Authentification with metamask, using the thirdweb @3rdweb/hooks package

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
+import "../styles/globals.css";
+
+// this represent the rinkeby test network chain
+const supportedChainIds = [4];
+
+const connectors = {
+  injected: {},
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebWeb3Provider
+      supportedChainIds={supportedChainIds}
+      connectors={connectors}
+    >
+      <Component {...pageProps} />
+    </ThirdwebWeb3Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
